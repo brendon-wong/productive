2b2f580d0f0ce02f97e897087f8035536845f81b
Brendon <brendonwbw@gmail.com>
2018-04-03 03:50:56 -0700

    Change notification code from testing to functional

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,36 +3,39 @@
     return true;
 }; */
 
-// Alerts
-// Subtle alert
-//setTimeout(function(){alert("Please check in with Productive.gq")}, 5000);
-// Intensive alert
-//setTimeout(function(){window.confirm("Please check in with Productive.gq")}, 5000);
-// Desktop Notifications
-//Push.create("Please check in with Productive.gq")
-// Play Audio
-//var a=new Audio('https://soundbible.com/grab.php?id=2156&type=mp3'); a.play();
-//setTimeout(function(){Push.create("Please check in with Productive.gq")}, 5000);
 
-// Prompt user for notification permissions if permissions not granted
+// Track if push notifications are enabled
 var can_push;
+// Prompt user for notification permissions if permissions not granted
+// This will call onGranted if notifications are granted, or onDenied if 
+// notifications are blocked or if the decision is deferred
 Push.Permission.request(onGranted, onDenied);
 
-function onGranted() {
-  var can_push = true;
-  setTimeout(function(){Push.create("Please check in with Productive.gq")}, 2000);
+// Trigger changes if notifications are granted
+function onGranted(callback) {
+  can_push = true;
 }
 
+// Trigger changes if notifications are denied/deferred
 function onDenied() {
-  var can_push = false;
-  var a=new Audio('https://soundbible.com/grab.php?id=2156&type=mp3'); a.play();
+  can_push = false;
 }
 
-/*
-if (Push.Permission.DEFAULT || Push.Permission.DENIED) {
-  Push.Permission.request();
-  // audio: http://soundbible.com/2156-Text-Message-Alert-3.html
-  var a=new Audio('https://soundbible.com/grab.php?id=2156&type=mp3'); a.play();
-} else {
-  setTimeout(function(){Push.create("Please check in with Productive.gq")}, 5000);
-} */
+/* Testing code 
+console.log("is false");
+setTimeout(function(){Push.create("Please check in with Productive.gq")}, 2000);
+// audio: http://soundbible.com/2156-Text-Message-Alert-3.html
+var a=new Audio('https://soundbible.com/grab.php?id=2156&type=mp3'); a.play();
+*/
+
+/* Alert code
+// Subtle alert
+setTimeout(function(){alert("Please check in with Productive.gq")}, 5000);
+// Intensive alert
+setTimeout(function(){window.confirm("Please check in with Productive.gq")}, 5000);
+// Desktop Notifications
+Push.create("Please check in with Productive.gq")
+// Play Audio
+var a=new Audio('https://soundbible.com/grab.php?id=2156&type=mp3'); a.play();
+setTimeout(function(){Push.create("Please check in with Productive.gq")}, 5000);
+*/
