252f98b130d980d075d80777fe9b9cecd7f2681e
Brendon <brendonwbw@gmail.com>
2018-04-03 06:26:58 -0700

    Add alert and alert timing logic

--- README.md
+++ README.md
@@ -1,10 +1,22 @@
 # Productive.gq
 
-Productive.gq is a web application with the purpose of helping users enter into a state of sustained productive work as soon as possible after the website is loaded. Productive.gq was created with HTML, CSS, JavaScript, and jQuery. It was designed to be run purely with frontend technologies and to be compatible with deployment on Github Pages.
+Productive.gq is a web application with the purpose of helping users overcome procrastination and work productively despite challenges to their attention. The application is centered around tracking user productivity with a Likert scale containing productivity levels from 1–7. Based on the user's response, or non-response, an adaptive notification system harnesses a combination of push notifications, audio alerts, and prominent computer-wide alerts to ensure users continue to be cognizant of their productivity and submit their productivity scores to the website.
 
 ## Features/User Stories
 
-- When the user loads the website, 
+- When the user loads the website, they immediately see a countdown timer that has already started labeled "Time Until Next Update."
+- Below the countdown timer is an input area called "Current Productivity" with seven large buttons labeled 1–7.
+- Whenever the countdown timer reaches zero, the user is alerted to submit a productivity update to the website.
+- If the user does not submit an update, the website continues to prompt the user for updates indefinitely every 2 minutes.
+- At the bottom of the page, there is a settings area which contains:
+    - Notification settings labeled: Visual, Audio, and Disruptive
+        - Visual: Push notifications for check-ins
+        - Audio: Audible alerts
+        - Disruptive: Browser alert boxes that interrupt internet browsing and other software the user is viewing to display an alert
+                
+## Future releases
+
+- In the future, the service will also support a pomodoro like system similar to Vitamin-R in which the user can enter a task and a timeblock, and alerts will occur throughout the time block to ensure the user remains on track. After timeblocks are completed, users will be able to extend their current timeblock or take a break.
 
 ## Style guide
 
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,78 @@
+// Notification settings
+var visual = true;
+var audio = true;
+var disruptive = true;
+
+// Alert settings 
+// var alert_interval = 2;
+var alert_interval;
+// Start with low productivity to activate alert interval faster if no response
+var current_productivity = 1;
+
+//var next_alert = new Date();
+//next_alert.setMinutes(now.getMinutes() + 30);
+
+function manager() {
+  alert_interval = calculate_alert_interval(current_productivity);
+  set_next_alert(alert_interval);
+}
+
+function trigger_alert() {
+  if (visual) {
+    Push.create("Please update Productive.gq");
+  }
+  if (audio) {
+    var a=new Audio('https://soundbible.com/grab.php?id=2156&type=mp3');
+    a.play();
+  }
+  if (disruptive) {
+    // Delay to allow audio to sound first
+    setTimeout(function(){window.alert("Please update Productive.gq")}, 500);
+  }
+}
+
+function set_next_alert(minutes) {
+  setTimeout(trigger_alert, (minutes * 60 * 1000));
+  setTimeout(manager, (minutes * 60 * 1000));
+}
+
+function calculate_alert_interval(current_productivity) {
+  let interval = 0.1;
+  return interval;
+}
+
+manager();
+
+
+
+
+
+
+
+
+
+
+
 // Display remain on page prompt; enable in production
 /* window.onbeforeunload = function() {
     return true;
 }; */
 
+//var msg = new SpeechSynthesisUtterance('Hello World');
+//window.speechSynthesis.speak(msg);
 
+// One line speech
+// window.speechSynthesis.speak(new SpeechSynthesisUtterance('Hello World'))
+
+
+
+/*
 // Track if push notifications are enabled
 var can_push;
 // Prompt user for notification permissions if permissions not granted
 // This will call onGranted if notifications are granted, or onDenied if 
 // notifications are blocked or if the decision is deferred
+// May be able to call Push.Permission.has(); for permissions status
 Push.Permission.request(onGranted, onDenied);
 
 // Trigger changes if notifications are granted
@@ -20,6 +84,9 @@ function onGranted(callback) {
 function onDenied() {
   can_push = false;
 }
+*/
+
+
 
 /* Testing code 
 console.log("is false");
@@ -32,7 +99,7 @@ var a=new Audio('https://soundbible.com/grab.php?id=2156&type=mp3'); a.play();
 // Subtle alert
 setTimeout(function(){alert("Please check in with Productive.gq")}, 5000);
 // Intensive alert
-setTimeout(function(){window.confirm("Please check in with Productive.gq")}, 5000);
+setTimeout(function(){window.alert("Please check in with Productive.gq")}, 4000);
 // Desktop Notifications
 Push.create("Please check in with Productive.gq")
 // Play Audio
