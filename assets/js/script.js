01b51e3a721daf5bc627778a0cabeff4e295bbfa
Brendon <brendonwbw@gmail.com>
2018-04-03 03:03:26 -0700

    Test notification permissions prompting

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,7 +9,7 @@
 // Intensive alert
 //setTimeout(function(){window.confirm("Please check in with Productive.gq")}, 5000);
 // Desktop Notifications
-Push.create("Please check in with Productive.gq")
+//Push.create("Please check in with Productive.gq")
 // Play Audio
 var a=new Audio('https://soundbible.com/grab.php?id=2156&type=mp3'); a.play();
 setTimeout(function(){Push.create("Please check in with Productive.gq")}, 5000);
