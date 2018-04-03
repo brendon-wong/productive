7c4b21ba229db3679108b75b5a438a24f7ef3cf0
Brendon <brendonwbw@gmail.com>
2018-04-03 00:00:50 -0700

    Add custom exit message

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,10 @@
-// Enable navigation prompt
+window.onbeforeunload = function(e) {
+  var dialogText = 'Remain productive and do not exit!';
+  e.returnValue = dialogText;
+  return dialogText;
+};
+
+/* Enable navigation prompt
 window.onbeforeunload = function() {
     return true;
-};
+}; */
