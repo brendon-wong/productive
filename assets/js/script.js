0cb2e6b4316de027fd6ad091e4a0371c248702fb
Brendon <brendonwbw@gmail.com>
2018-04-03 03:12:41 -0700

    Add notification permissions check before prompting

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,5 +11,12 @@
 // Desktop Notifications
 //Push.create("Please check in with Productive.gq")
 // Play Audio
-var a=new Audio('https://soundbible.com/grab.php?id=2156&type=mp3'); a.play();
-setTimeout(function(){Push.create("Please check in with Productive.gq")}, 5000);
+//var a=new Audio('https://soundbible.com/grab.php?id=2156&type=mp3'); a.play();
+//setTimeout(function(){Push.create("Please check in with Productive.gq")}, 5000);
+
+// Prompt user for notification permissions if permissions not granted
+if (Push.Permission.DEFAULT || Push.Permission.DENIED) {
+  Push.Permission.request();
+} else {
+  var a=new Audio('https://soundbible.com/grab.php?id=2156&type=mp3'); a.play();
+}
