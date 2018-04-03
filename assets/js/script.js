d70c870a3642a278ee3bc315d31daceb0581bb2a
Brendon <brendonwbw@gmail.com>
2018-04-03 03:20:30 -0700

    Update notification permissions check

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,8 +15,23 @@
 //setTimeout(function(){Push.create("Please check in with Productive.gq")}, 5000);
 
 // Prompt user for notification permissions if permissions not granted
-if (Push.Permission.DEFAULT || Push.Permission.DENIED) {
-  Push.Permission.request();
-} else {
+Push.Permission.request(onGranted, onDenied);
+
+function onGranted() {
+  var can_push = true;
+  setTimeout(function(){Push.create("Please check in with Productive.gq")}, 5000);
+}
+
+function onDenied() {
+  var can_push = false;
   var a=new Audio('https://soundbible.com/grab.php?id=2156&type=mp3'); a.play();
 }
+
+/*
+if (Push.Permission.DEFAULT || Push.Permission.DENIED) {
+  Push.Permission.request();
+  // audio: http://soundbible.com/2156-Text-Message-Alert-3.html
+  var a=new Audio('https://soundbible.com/grab.php?id=2156&type=mp3'); a.play();
+} else {
+  setTimeout(function(){Push.create("Please check in with Productive.gq")}, 5000);
+} */
