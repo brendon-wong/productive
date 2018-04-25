86b3f501f7bd50ab792d4b6f93355d17fa1312b5
Brendon <brendonwbw@gmail.com>
2018-04-24 22:33:52 -0700

    Complete goals page

--- assets/js/goals.js
+++ assets/js/goals.js
@@ -4,20 +4,32 @@ $(document).ready(function() {
   $("#new-goal").keypress( function(e) {
     if (e.which == 13 || e.keyCode == 13) {
       newGoal = $("#new-goal").val();
-      $("#goals-container").append('<div class="goal">' + newGoal + '</div>');
+      // jQuery Function Number 1
+      $("#goals-container").append('<div class="goal">' + newGoal + '<i class="fas fa-times"></i></div>');
       $("#new-goal").val("");
     }
   });
   
-  $(".goal").click( function() {
+  // jQuery Function Number 2
+  $(document).on('click', '.goal', function() {
+    // jQuery Function Number 3
     if ($(this).hasClass("complete")) {
+      // jQuery Function Number 4
       $(this).removeClass("complete");
+      // jQuery Function Number 5
       $(this).children(".fa-check").remove();
     }
     else {
+      // jQuery Function Number 6
       $(this).addClass("complete");
+      // jQuery Function Number 7
       $(this).prepend('<i class="fas fa-check"></i>');
-    }
+    }  
+  });
+  
+  $(document).on('click', '.fa-times', function() {
+    // jQuery Function Number 8
+    $(this).parent().remove();
   });
 
   /*
